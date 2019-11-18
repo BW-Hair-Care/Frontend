417cc6e83092cdf86132c8f370ceda5052c63486
Afia-Lydia <you@example.com>
2019-11-18 16:23:32 -0500

    fixed error, installed dependencies in correct folder

--- hair-care/src/index.js
+++ hair-care/src/index.js
@@ -8,10 +8,10 @@ import store from "./redux/store";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-	// <Provider store={store} >
+	<Provider store={store} >
 		<Router>
 			<App />
 		</Router>
-	// </Provider>,
-	,rootElement
+	 </Provider>,
+	rootElement
 );
