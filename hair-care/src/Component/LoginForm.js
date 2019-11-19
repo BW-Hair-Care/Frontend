499701470c88fd7bfa64a8428a56ff99f34be176
Afia-Lydia <you@example.com>
2019-11-19 10:59:46 -0500

    created component for registration.

--- hair-care/src/Component/LoginForm.js
+++ hair-care/src/Component/LoginForm.js
@@ -1,24 +1,27 @@
 import React, { useState } from "react";
 
 export function LoginForm() {
-  const [user, setUser] = useState("");
+	const [user, setUser] = useState({
+		username: "",
+		password: ""
+	});
 
-  const handleChange = e => {
-    const { value, name } = e.target;
+	const handleChange = e => {
+		const { value, name } = e.target;
 
-    setUser({ [name]: value });
-  };
+		setUser(...user, { [name]: value });
+	};
 
-  const submitForm = event => {
-    event.preventDefault();
-  };
+	const submitForm = event => {
+		event.preventDefault();
+	};
 
-  return (
-    <form onSubmit={submitForm}>
-      <label htmlFor="user-login">User Login</label>
-      <input name="username" type="text" onChange={handleChange} />
-      <label htmlFor="password">Password</label>
-      <input name="password" type="text" onChange={handleChange} />
-    </form>
-  );
+	return (
+		<form onSubmit={submitForm}>
+			<label htmlFor="user-login">User Login</label>
+			<input name="username" type="text" onChange={handleChange} />
+			<label htmlFor="password">Password</label>
+			<input name="password" type="text" onChange={handleChange} />
+		</form>
+	);
 }
